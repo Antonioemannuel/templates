dca8f265b5b36ac02cfc15a17902f2ab32939342
Antonioemannuel <antonioemannuel13@gmail.com>
2023-01-20 18:27:53 -0300

    criado view

---
+++ resources/views/portifolio/parceiros.blade.php
@@ -0,0 +1,65 @@
+<!--====== Start Partners Section ======-->
+<section class="partners-area">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="section-title text-center mb-55 fadeInDown">
+                    <h2>We’ve 1534+ Global Partners</h2>
+                    <p>Professional Design Agency to provide solutions</p>
+                </div>
+            </div>
+        </div>
+        <div class="partners-wrapper wow fadeInUp">
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-1.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-2.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-3.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-4.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-5.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-6.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-7.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-8.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-9.png" alt=""></a>
+                </div>
+            </div>
+            <div class="partner-item">
+                <div class="img-holder">
+                    <a href="#"><img src="assets/images/partners/img-10.png" alt=""></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!--====== End Partners Section ======-->
