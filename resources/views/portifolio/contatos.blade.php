8a1bc5cac8e296a42e11a09b104205037cbf0c3c
Antonioemannuel <antonioemannuel13@gmail.com>
2023-01-20 18:26:00 -0300

    criado view

---
+++ resources/views/portifolio/contatos.blade.php
@@ -0,0 +1,75 @@
+<!--====== Start Contact Section ======-->
+<section class="contact-area contact-area-v2 pt-80 pb-125">
+    <div class="container-1350">
+        <div class="contact-wrapper pt-70 pb-40">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="text-wrapper mb-40 wow fadeInLeft">
+                        <h2>Have Any Project 
+                            On Minds!</h2>
+                        <p>On the other hand denounce righteous indigna
+                                tion and dislike men who are beguiled and demra
+                                charms of pleasure of the moment</p>
+                        <div class="information-style-two d-flex mb-20">
+                            <div class="icon">
+                                <i class="far fa-envelope-open"></i>
+                            </div>
+                            <div class="info">
+                                <h5 class="blue-dark">Email Us</h5>
+                                <h4><a href="mailto:support@gmail.com">support@gmail.com</a></h4>
+                            </div>
+                        </div>
+                        <div class="information-style-two d-flex mb-20">
+                            <div class="icon">
+                                <i class="far fa-phone"></i>
+                            </div>
+                            <div class="info">
+                                <h5 class="blue-dark">Phone Us</h5>
+                                <h4><a href="tel:+012(345)6789">+012 (345) 6789</a></h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="contact-form mb-40 wow fadeInRight">
+                        <h2>Get Consultation
+                            Say Hello!</h2>
+                        <form>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form_group">
+                                        <input type="text" class="form_control" placeholder="Full Name" name="name" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form_group">
+                                        <input type="email" class="form_control" placeholder="Email Address" name="email" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form_group">
+                                        <select class="wide">
+                                            <option data-display="Subject">Subject</option>
+                                            <option value="01">Customer Support</option>
+                                            <option value="02">Customer Support</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form_group">
+                                        <input type="text" class="form_control" placeholder="Phone Number" name="phone" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form_group">
+                                        <button class="main-btn arrow-btn">Send Message</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!--====== End Contact Section ======-->
